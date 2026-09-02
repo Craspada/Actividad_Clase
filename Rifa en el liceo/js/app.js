a17f7feb7d7b0aa7eb3aa83b3e243d84fea3065a
Craspada <143526393+Craspada@users.noreply.github.com>
2026-09-02 09:27:20 -0400

    Add files via upload
    hecho varias cosas

--- Rifa en el liceo/js/app.js
+++ Rifa en el liceo/js/app.js
@@ -1,6 +1,26 @@
+// Manejo del menú responsivo
 const botonMenu = document.getElementById('boton_menu');
 const nav = document.querySelector('nav');
 
 botonMenu.addEventListener('click', () => {
     nav.classList.toggle('nav-activo');
 });
+
+// Manejo del catálogo de servicios
+const equipo = [
+    { nombre: "Kevin Trujillo", curso: "4°D", frase: "Nos haremos ricos con esta ****", foto: "img/ProgramadorPromedio.avif" }
+];
+
+const divEquipo = document.getElementById('equipo');
+if (divEquipo) {
+    equipo.forEach(persona => {
+        divEquipo.innerHTML += `
+            <div class="tarjeta">
+                <img src="${persona.foto}" alt="${persona.nombre}">
+                <h3>${persona.nombre}</h3>
+                <p>${persona.curso} - "${persona.frase}"</p>
+            </div>`;
+    });
+}
+
+
